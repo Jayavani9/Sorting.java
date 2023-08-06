b1fc2118edaabf6c50bab4a05554e061e8f32452
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-05 21:12:55 -0500

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,1 +1,5 @@
-
+Implement a function that takes an array of integers as input and returns a sorted version of the array using the Selection Sort algorithm.
+Sample Input
+array = [8, 5, 2, 9, 5, 6, 3]
+Sample Output
+[2, 3, 5, 5, 6, 8, 91
