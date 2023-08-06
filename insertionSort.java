9e3082cdb5e24370f53441c722cf586e20510686
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-05 21:07:59 -0500

    Update insertionSort.java

--- insertionSort.java
+++ insertionSort.java
@@ -1,1 +1,6 @@
+Implement a function that takes an array of integers as input and returns a sorted version of the array using the Insertion Sort algorithm.
+Sample Input
+array = (8, 5, 2, 9, 5, 6, 31
+Sample Output
+[2, 3, 5, 5, 6, 8, 9]
 
